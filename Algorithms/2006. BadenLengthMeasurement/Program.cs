f8b6f9433d78130e95396ed3ab0414bc47cd440a
Alex Golubev <fir258@yandex.ru>
2022-06-16 18:44:24 +0400

    2007

---
+++ Algorithms/2006. BadenLengthMeasurement/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2006.BadenLengthMeasurement
+{
+    public class Program
+    {
+        public static int EnterLength()
+        {
+            int length = 0;
+
+            do
+            {
+                Console.Write("Enter a length in cm (1-10000): ");
+                int.TryParse(Console.ReadLine(), out length);
+            }
+            while (length < 1 || length > 10000);
+
+            return length;
+        }
+
+        public static int ConvertCmToInches(int length)
+        {
+            if(length < 1)
+            {
+                return 0;
+            }
+
+            int inch = length / 3;
+
+            if(length % 3 == 2)
+            {
+                inch++;
+            }
+
+            return inch;
+        }
+
+        public static int ConvertInchesToFeet(ref int inch)
+        {
+            if (inch < 1)
+            {
+                return 0;
+            }
+
+            int foot = inch / 12;
+            inch = inch % 12;
+
+            return foot;
+        }
+
+        public static void Main(string[] args)
+        {
+            int length = 0;
+            int inch = 0;
+            int foot = 0;
+
+            Console.WriteLine("Length measurement in Baden (1 foot = 12 inch, 1 inch = 3 cm)");
+
+            length = EnterLength();
+            inch = ConvertCmToInches(length);
+            foot = ConvertInchesToFeet(ref inch);
+
+            Console.WriteLine("Result (feet, inches): " + foot + " " + inch);
+        }
+    }
+}
---
+++ Algorithms/2007. RoundNumbersInBinarySystem/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2007.RoundNumbersInBinarySystem
+{
+    public class Program
+    {
+        public static int EnterNumber()
+        {
+            int number = 0;
+            int rangeTo = (int) Math.Pow(10, 9);
+
+            do
+            {
+                Console.Write("Enter a number (1-10^9): ");
+                int.TryParse(Console.ReadLine(), out number);
+            }
+            while (number < 1 || number > rangeTo);
+
+            return number;
+        }
+
+        public static int RoundnessDetermination(int number)
+        {
+            int round = 0;
+
+            while(number % 2 == 0)
+            {
+                round++;
+                number /= 2;                
+            }
+
+            return round;
+        }
+
+        public static void Main(string[] args)
+        {
+            int number = 0;
+
+            Console.WriteLine("Round numbers in the binary system (the max power of two that the number is divisible by)");
+
+            number = EnterNumber();
+            Console.WriteLine();
+
+            Console.WriteLine("Result: " + RoundnessDetermination(number));
+        }
+    }
+}
